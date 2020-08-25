9d475d16563d73e2fc350a198b10433938313f11
metrog <metrog950@gmail.com>
2020-08-25 21:38:48 +0900

    BOJ_N과 M(6)_BackTracking

---
+++ Baekjoon/BackTracking/15655-N과 M(6).cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <math.h>
+#include <queue>
+#include <algorithm>
+#include <string.h>
+#include<stdio.h>
+#include<stack>
+
+using namespace std;
+int a, b;
+int visit[9] = { 0, };
+vector<int> v;
+vector<int> num;
+
+void dfs() {
+	if (v.size() == b) {
+		for (int i = 0;i < v.size();i++) {
+			cout << v[i] << ' ';
+
+		}
+		cout << '\n';
+		return;
+	}
+	for (int i = 0;i < num.size();i++) {
+		if (!visit[i]) {
+			if (v.empty()) {
+				v.push_back(num[i]);
+				visit[i] = 1;
+				dfs();
+				v.pop_back();
+				visit[i] = 0;
+			}
+			else if (v[v.size() - 1] < num[i]) {
+				v.push_back(num[i]);
+				visit[i] = 1;
+				dfs();
+				v.pop_back();
+				visit[i] = 0;
+			}
+		}
+
+	}
+}
+int main() {
+	cin >> a >> b;
+	int num2;
+	for (int i = 0;i < a;i++) {
+		cin >> num2;
+		num.push_back(num2);
+	}
+	sort(num.begin(), num.end());
+	dfs();
+}
