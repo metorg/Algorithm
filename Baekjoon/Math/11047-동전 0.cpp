6bc8333fd7743a6d1f4c52c79b62f4a8ab306b21
metrog <metrog950@gmail.com>
2021-01-20 18:37:34 +0900

    BOJ_동전 0(Math)

---
+++ Baekjoon/Math/11047-동전 0.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <string>
+#include <sstream>
+#include<set>
+
+using namespace std;
+
+
+int main() {
+	int n, num, target;
+	int cnt = 0;
+	vector<int> coin;
+
+	cin >> n >> target;
+	for (int i = 0;i < n;i++) {
+		cin >> num;
+		coin.push_back(num);
+	}
+
+	sort(coin.begin(), coin.end());
+
+	while (target) {
+		int i = 0;
+
+		for (i = 0;i<coin.size();i++)
+			if (coin[i] > target)break;
+		
+
+		if (i == 0)target -= coin[0];
+		else target -= coin[i-1];
+		cnt++;
+	}
+	cout << cnt;
+
+}
