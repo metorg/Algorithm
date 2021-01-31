1408df3cab8390368c1c7c68dc631a30a0abb5c2
metrog <metrog950@gmail.com>
2021-01-31 23:58:22 +0900

    BOJ_평범한 배낭(DP) Comment : 원래는 2차원 배열을 사용하여 풀었던 문제를 알고리즘의 수정을 통해 1차원배열을 사용하여 메모리를 아낄수 있게끔 다시 구현하였다.

--- Baekjoon/Dp/12865-평범한 배낭(DP).cpp
+++ Baekjoon/Dp/12865-평범한 배낭(DP).cpp
@@ -12,32 +12,27 @@
 
 using namespace std;
 
-int n,m,weight,value;
+int n, m, weight, value;
 vector<pair<int, int>> v;
-int dp[101][100001];
-
+int dp[100001];
+int ans = 0;
 int main() {
 
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 	cin >> n >> m;
-
 	v.push_back({ 0,0 });
 
 	for (int i = 0;i < n;i++) {
 		cin >> weight >> value;
-		v.push_back({weight, value});
+		v.push_back({ weight, value });
 	}
-	
-	for (int i = 1;i <= n;i++) {
-		for (int j = 1;j <= m;j++) {
 
-			if (j >= v[i].first) dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - v[i].first] + v[i].second);
-			else dp[i][j] = dp[i - 1][j];
-
-		}
+	for (int i = 0;i < v.size();i++) {
+		for (int j = m;j >= v[i].first; j--) dp[j] = max(dp[j], dp[j - v[i].first] + v[i].second);
 	}
-	cout << dp[n][m];
+
+	cout << dp[m];
 
 }
