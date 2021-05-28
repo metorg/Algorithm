87ac98b5814797b255940314a320acf951bed575
DESKTOP-JTS3MTU\dlgud <metrog950@gmail.com>
2021-05-28 23:20:19 +0900

    POG_단속카메라(Greedy)

---
+++ Programmers/Greedy/단속카메라(Greedy).cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<vector<int>> routes) {
+    
+    int answer = 1;
+    sort(routes.begin(),routes.end());
+    int end=routes[0][1];
+    for(int i=1;i<routes.size();i++){
+        if(end<routes[i][0]){
+            answer++;
+            end=routes[i][1];
+        }
+        else if(end>=routes[i][1]){
+            end=routes[i][1];
+        }
+         
+    }
+    return answer;
+}
