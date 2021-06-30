15ef6e975c15e00ee39c44e9d9597d0d67c2f797
DESKTOP-BTO9KQS\leeseungmin <metrog950@gmail.com>
2021-06-30 13:50:37 +0900

    feat: 코드 수정

--- C++/Baekjoon/BruteForce/17135-캐슬 디펜스(Brute).cpp
+++ C++/Baekjoon/BruteForce/17135-캐슬 디펜스(Brute).cpp
@@ -9,53 +9,58 @@
 #include<stack>
 
 using namespace std;
-int board[16][16];
-int c_board[16][16];
+
+int board[15][15];
+int c_board[15][15];
+
 vector<int> v;
+
 int n, m, dist;
-int bfs() {
+
+int brute_force() {
+	
 	vector<pair<int, int>> target;
 	int answer = 0;
-	int t = n+1;
+	int col = n+1;
 	int line = 0;
+	
 	while (1) {
-		t--;
-		if (t == -1)break;
+
+		col--;
+		if (col == -1)break;
+
 		for (int i = 0; i < v.size(); i++) {
-			int limit = 99;
-			int cnt = t;
+
+			int limit = dist;
+			int c_col = col;
 			int target_x = 99;
 			int target_y = 99;
+
 			while (1) {
-				cnt--;
-				if (cnt == -1)break;
+				c_col--;
+				if (c_col == -1)break;
 				for (int j = 0; j < m; j++) {
-					if (c_board[cnt][j] == 1) {
-						int r = abs((n-line) - cnt) + abs(v[i] - j);
-						if (r <= dist ) {
-							if (limit > r) {
-								limit = r;
-								target_x = cnt;
-								target_y = j;
-							}
-							else if (limit == r) {
-								if (target_y > j) {
-									target_x = cnt;
-									target_y = j;
-								}
-							}
+					if (c_board[c_col][j] == 1) {
+						int r = abs(n-line - c_col) + abs(v[i] - j);
+						
+						if (limit > r) {
+							limit = r;
+							target_x = c_col;
+							target_y = j;
+						}
+						else if (limit == r&&target_y>j) {
+							target_x = c_col;
+							target_y = j;
 						}
 					}
 				}
 			}
+			
 			if (target_x == 99 || target_y == 99)continue;
-			else {
-				target.push_back({ target_x,target_y });
-				
-			}
+			else target.push_back({ target_x,target_y });						
 		}
 		for (int p = 0; p < target.size(); p++) {
-			if (c_board[target[p].first][target[p].second] == 1) {
+			if (c_board[target[p].first][target[p].second]) {
 				answer++;
 				c_board[target[p].first][target[p].second] = 0;
 			}
@@ -69,11 +74,13 @@ int bfs() {
 int main(){
 	int last = 0;
 	cin >> n >> m >> dist;
+	
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
 			cin >> board[i][j];
 		}
 	}
+	
 	for (int i = 0; i < m-2; i++) {
 		v.push_back(i);
 		for (int j = i + 1; j < m-1; j++) {
@@ -81,7 +88,7 @@ int main(){
 			for (int k = j + 1; k < m; k++) {
 				v.push_back(k);
 				memcpy(c_board, board, sizeof(board));
-				last=max(last,bfs());
+				last=max(last,brute_force());
 				v.pop_back();
 			}
 			v.pop_back();
