c592d418c219dd254c6faa93d3b9befe14fbc7bc
DESKTOP-BTO9KQS\leeseungmin <metrog950@gmail.com>
2021-07-23 14:08:08 +0900

    BOJ_공주를 지켜라!(BFS) feat: 코드 복사 오류

--- C++/Baekjoon/BFS&&DFS/17836-공주를 지켜라!(BFS).cpp
+++ C++/Baekjoon/BFS&&DFS/17836-공주를 지켜라!(BFS).cpp
@@ -1,55 +1,80 @@
-#include <iostream>
-#include <queue>
-#include <vector>
-#include <algorithm>
-#include <unordered_map>
 #include <string>
-#include <sstream>
-#include<stdio.h>
+#include <vector>
+#include <unordered_map>
+#include <iostream>
+#include <set>
+#include <map>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include<cstring>
+#include<stack>
 #include<string.h>
-#include<set>
+#include<stdio.h>
 
 using namespace std;
-int n,num1,num2;
 
-vector<vector<int>> v(100001);
-int answer[100001];
+int n, m, t;
+int board[102][102];
+int visit[102][102][2];
+int xx[4] = { -1,1,0,0 };
+int yy[4] = { 0,0,-1,1 };
 
-int bfs(int start) {
-	
-	queue<int> q;
-	q.push(start);
-	answer[start] = 1;
+
+typedef struct {
+	int x;
+	int y;
+	int dist;
+	int sword;
+}Info;
+
+int bfs() {
+
+	queue<Info> q;
+	q.push({ 0,0,0,0 });
+	visit[0][0][0] = 1;
 
 	while (!q.empty()) {
-		
-		int num = q.front();	
+		int x = q.front().x;
+		int y = q.front().y;
+		int dist = q.front().dist;
+		int sword = q.front().sword;
 		q.pop();
 
-		for (int i = 0;i < v[num].size();i++) {
-			int next = v[num][i];
-			if (answer[next])continue;
-			answer[next] = num;
-			q.push(next);
+		if (board[x][y] == 2) sword = 1;
+		if (x == n - 1 && y == m - 1)return dist;
+		
+		for (int i = 0; i < 4; i++) {		
+			int c_x = x + xx[i];
+			int c_y = y + yy[i];
+			if (c_x < 0 || c_y < 0 || c_x >= n || c_y >= m || visit[c_x][c_y][sword] == 1 || dist + 1>t)continue;
+			if (board[c_x][c_y] == 1 && sword == 0)continue;
+
+			q.push({ c_x,c_y,dist + 1,sword });
+			visit[c_x][c_y][sword] = 1;
 		}
 	}
 	return 0;
-}
-int main() {
-	
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	
-	cin >> n;
-	for (int i = 0;i < n-1;i++) {
-		cin >> num1 >> num2;
-		v[num1].push_back(num2);
-		v[num2].push_back(num1);
-	}
-	bfs(1);
-	for (int i = 2;i <= n;i++) {
-		cout << answer[i] << '\n';
-	}
+
 }
 
+int main() {
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int answer;
+	cin >> n >> m >> t;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> board[i][j];
+		}
+	}
+	answer = bfs();
+	if (answer == 0) {
+		cout << "Fail";
+	}
+	else {
+		cout << answer;
+	}
+}
