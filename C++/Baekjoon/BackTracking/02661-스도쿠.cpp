e182b5fbc1ebc9cb867dfd50c9bbb98b9c9bfe12
DESKTOP-BTO9KQS\leeseungmin <metrog950@gmail.com>
2021-10-06 19:34:20 +0900

    BOJ_스도쿠(BackTracking)(복습)

--- C++/Baekjoon/BackTracking/02661-스도쿠.cpp
+++ C++/Baekjoon/BackTracking/02661-스도쿠.cpp
@@ -1,59 +1,72 @@
-#include<string>
+#include <iostream>
 #include <vector>
-#include<queue>
-#include<iostream>
-#include<algorithm>
-#include<cstdio>
-
+#include <math.h>
+#include <queue>
+#include <algorithm>
+#include <string.h>
+#include<stdio.h>
+#include<stack>
+#include<memory.h>
+#include<unordered_map>
+#include<set>
 using namespace std;
-int n = 9;
-int board[10][10];//스도쿠판
-int visit[10][10] = { 0, };//행중복을 체크하는 방문벡터
-int visit2[10][10] = { 0, };//열중복을 체크하는 방문벡터
-int Square[10][10] = { 0, };//스도쿠표를 9칸씩 쪼개서 체크하는 방문벡터
-vector<pair<int, int>> v;//0인 값들을 저장하는 벡터
 
-void dfs(int depth) {
-	if (depth == v.size()) {
-		for (int i = 1;i <= n;i++) {
-			for (int j = 1;j <= n;j++) {
+int board[10][10];
+int col_visit[10][10];
+int row_visit[10][10];
+int square_visit[10][10];
+vector<pair<int, int>> zero;
+int cut = 0;
+
+void sudoku(int depth) {
+	if (depth == zero.size()) {
+		for (int i = 1; i <= 9; i++) {
+			for (int j = 1; j <= 9; j++) {
 				cout << board[i][j] << ' ';
-
 			}
 			cout << '\n';
 		}
 		exit(0);
-	};
-	for (int i = 1;i <= n;i++) {
-		if (!visit[v[depth].first][i] && !visit2[i][v[depth].second] && (!Square[((v[depth].first - 1) / 3) * 3 + ((v[depth].second - 1) / 3)][i])) {
-			visit[v[depth].first][i] = 1;
-			visit2[i][v[depth].second] = 1;
-			Square[((v[depth].first - 1) / 3) * 3 + ((v[depth].second - 1) / 3)][i] = 1;
-			board[v[depth].first][v[depth].second] = i;
-			dfs(depth + 1);
-			visit[v[depth].first][i] = 0;
-			visit2[i][v[depth].second] = 0;
-			Square[((v[depth].first - 1) / 3) * 3 + ((v[depth].second - 1) / 3)][i] = 0;
-
-		}
 	}
-}
 
+	for (int j = 1; j <= 9; j++) {
+		if (row_visit[zero[depth].first][j] || col_visit[j][zero[depth].second] || square_visit[((zero[depth].first - 1) / 3) * 3 + ((zero[depth].second - 1) / 3)][j])continue;
+		row_visit[zero[depth].first][j] = 1;
+		col_visit[j][zero[depth].second] = 1;
+		square_visit[((zero[depth].first - 1) / 3) * 3 + ((zero[depth].second - 1) / 3)][j] = 1;
+		board[zero[depth].first][zero[depth].second] = j;
+		sudoku(depth + 1);
+		row_visit[zero[depth].first][j] = 0;
+		col_visit[j][zero[depth].second] = 0;
+		square_visit[((zero[depth].first - 1) / 3) * 3 + ((zero[depth].second - 1) / 3)][j] = 0;
+		board[zero[depth].first][zero[depth].second] = 0;
+	}
+
+
+}
 int main() {
 
-	for (int i = 1;i <= n;i++) {
-		for (int j = 1;j <= n;j++) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	for (int i = 1; i <= 9; i++) {
+		for (int j = 1; j <= 9; j++) {
 			cin >> board[i][j];
-			if (board[i][j] == 0) {
-				v.push_back({ i,j });
-			}
+			if (board[i][j] == 0)zero.push_back({ i,j });
 			else {
-				visit[i][board[i][j]] = 1;
-				visit2[board[i][j]][j] = 1;
-				Square[((i - 1) / 3) * 3 + ((j - 1) / 3)][board[i][j]] = 1;
+				row_visit[i][board[i][j]] = 1;
+				col_visit[board[i][j]][j] = 1;
+				square_visit[((i - 1) / 3) * 3 + ((j - 1) / 3)][board[i][j]] = 1;
 			}
 		}
 	}
-	dfs(0);
+	sudoku(0);
+	for (int i = 1; i <= 9; i++) {
+		for (int j = 1; j <= 9; j++) {
+			cout << board[i][j] << ' ';
+		}
+		cout << '\n';
+	}
 
 }
